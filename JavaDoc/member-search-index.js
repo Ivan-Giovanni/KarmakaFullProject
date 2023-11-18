9509b29599e767798d0f01ea7cafe713c1bda284
Ivan-Giovanni <zanguegiovanni@gmail.com>
2023-11-18 06:36:27 +0100

    J'ai crée la JavaDoc

---
+++ JavaDoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"Cards","c":"Card","l":"Card()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ src/Cards/Card.java
@@ -0,0 +1,11 @@
+package Cards;
+
+/**
+ * @author giovannizangue
+ * @version 1.0
+ *
+ * <br>
+ * Cette classe contient les proprietes communes a toutes les 64 cartes du jeu
+ */
+public abstract class Card {
+}
