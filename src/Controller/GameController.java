ed02388b9844fe8fcaca56d97761a7cc25c89bd4
Ivan-Giovanni <zanguegiovanni@gmail.com>
2023-11-21 17:55:27 +0100

    Creation des classes 3

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -3,13 +3,4 @@ package Controller;
 public class GameController {
 
 
-    //============================================================================================//
-    // ====================================== MAIN ============================================ //
-    public static void main(String[] args) {
-        System.out.println();
-
-        System.out.println("Début de la partie...");
-
-
-    }
 }
