ce1cb615c17c4d721e978f4d3271f7831c889710
Ivan-Giovanni <zanguegiovanni@gmail.com>
2023-11-21 18:55:26 +0100

    Creation des classes 3

--- src/Model/Joueur/CollectionsDeCartes/Oeuvre.java
+++ src/Model/Joueur/CollectionsDeCartes/Oeuvre.java
@@ -2,27 +2,44 @@ package Model.Joueur.CollectionsDeCartes;
 
 import Model.Cards.Card;
 
+import java.util.List;
+
 /**
  * @author giovannizangue
  * Cette classe correspond à l'oeuvre du Model.Joueur
  */
 public class Oeuvre extends CollectionDeCartes{
 
+    // =============================================== LES ATTRIBUTS ============================================ //
+    private List<Card> oeuvre;
+
+    // ============================================= LE CONSTRUCTEUR ============================================ //
+    public Oeuvre() {
+
+    }
+
+    // =============================================== LES GETTERS ============================================ //
+    public List<Card> getOeuvre() {
+        return oeuvre;
+    }
+
 
     // ========================================= LES METHODES ============================================ //
+
+    // ========================================= ADD & REMOVE ============================================ //
+    @Override
+    public void addCard(Card carte) {
+        oeuvre.add(carte);
+    }
+
+    @Override
+    public Card removeCard() {
+        return oeuvre.remove(0);
+    }
 
     // ================================= CALCULER NOMBRE DE POINTS ======================================= //
     public void CalculerNombreDePoints() {
 
     }
 
-    @Override
-    public void addCard(Card carte) {
-
-    }
-
-    @Override
-    public Card removeCard() {
-        return null;
-    }
 }
--- src/Model/Joueur/CollectionsDeCartes/VieFuture.java
+++ src/Model/Joueur/CollectionsDeCartes/VieFuture.java
@@ -2,12 +2,27 @@ package Model.Joueur.CollectionsDeCartes;
 
 import Model.Cards.Card;
 
+import java.util.List;
+
 /**
  * @author giovannizangue
  * <br>
  * Cette classe correspond à la Vie Future du Model.Joueur
  */
-public class VieFuture extends CollectionDeCartes{
+public class VieFuture extends CollectionDeCartes {
+
+    // =============================================== LES ATTRIBUTS ============================================ //
+    private List<Card> vieFuture;
+
+    // =============================================== LES GETTERS ============================================ //
+    private List<Card> getVieFuture() {
+        return vieFuture;
+    }
+
+
+    // ============================================= LES METHODES ============================================ //
+
+    // ============================================= ADD & REMOVE ============================================ //
     @Override
     public void addCard(Card carte) {
 
