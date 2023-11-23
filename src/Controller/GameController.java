e3fe944b70900d515e3432f2454f6e981b658ef1
Ivan-Giovanni <zanguegiovanni@gmail.com>
2023-11-23 08:05:23 +0100

    Creation des classes 4

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -1,6 +1,75 @@
 package Controller;
 
+import Model.Partie;
+import View.GameView;
+
+/**
+ * @author giovannizangue
+ * Cette classe correspond au controller de notre jeu
+ */
 public class GameController {
+
+    // =============================================== LES ATTRIBUTS ========================================== //
+    GameView view;
+
+    // ============================================ LE CONSTRUCTEUR ========================================= //
+    public GameController(GameView wiew, Partie partie) {
+
+    }
+
+    // =============================================== LES METHODES ========================================== //
+
+    public void run() {
+
+    }
+
+    public void creerLaPartie() {
+
+    }
+
+    public void ajouterLesJoueurs() {
+
+    }
+
+    public void commencerLaPartie() {
+
+    }
+
+    public void creerLaSource() {
+
+    }
+
+    public void shuffleLaSource() {
+
+    }
+
+    public void creerLaFosse() {
+
+    }
+
+    public void creerLesAnneauxKarmiques() {
+
+    }
+
+    public void distribuerLesCartesDeLaMain() {
+
+    }
+
+    public void distribuerLesCartesDeLaPile() {
+
+    }
+
+    public void nextAction() {
+        /* Sucession de switch case, d'action performed, etc... */
+    }
+
+    public void determinerLeGagnant() {
+
+    }
+
+    public void terminerLaPartie() {
+        /* Voulez vous rejouer une nouvelle partie? */
+    }
 
 
 }
